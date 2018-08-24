387dc939b2376da3f9083949942961ebafb35572
FabioTessarollo <S227901@studenti.polito.it>
2018-08-24 16:31:30 +0200

    a

--- GettingRSSI.py
+++ GettingRSSI.py
@@ -8,23 +8,22 @@ class ScanDelegate(DefaultDelegate):
         if isNewDev:
             print "Dispositivo rilevato", dev.addr
         elif isNewData:
-            print "Received new data from", dev.addr
+            print "Dati rilevati da", dev.addr
 
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 
 
 def calcolaDistanza(rssi):
+    txpower = -59
     if rssi == 0:
         return -1
     else:
-        ratio = rssi / -59
+        ratio = rssi / txpower
         if ratio < 1:
             return ratio ** 10
         else:
             return 0.89976 * ratio ** 7.7095 + 0.111
 
 for dev in devices:
-    print "Dispositivo %s (%s), RSSI=%d dB, Distanza=%d m" % (dev.addr, dev.addrType, dev.rssi, calcolaDistanza(dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+    print "Dispositivo %s (%s), RSSI=%d dB, Distanza=%f m" % (dev.addr, dev.addrType, dev.rssi, calcolaDistanza(dev.rssi))
